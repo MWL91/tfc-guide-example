9807f1e66c682b64162374fa96be4650e801f5dc
Marcin Lenkowski <marcin@lenkowski.net>
2022-11-09 11:10:27 +0100

    bucket

--- main.tf
+++ main.tf
@@ -2,27 +2,12 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+resource "aws_s3_bucket" "a" {
+  bucket = "mwl-bucket-aws"
 
   tags = {
-    Name = var.instance_name
+    Name        = "My bucket"
+    Environment = "Dev"
+    CostCenter  = "1234"
   }
 }
